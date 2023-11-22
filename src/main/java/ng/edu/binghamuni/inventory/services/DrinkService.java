8af5de4da5a6108973456b2e01444b1e98861fcf
samuelolubukun <samuel.olubukun@binghamuni.edu.ng>
2023-11-22 16:31:08 +0100

    Update: repository, controller and services

---
+++ src/main/java/ng/edu/binghamuni/inventory/services/DrinkService.java
@@ -0,0 +1,14 @@
+package ng.edu.binghamuni.inventory.services;
+
+import ng.edu.binghamuni.inventory.Domain.Drink;
+
+import java.util.List;
+
+public interface DrinkService {
+    Drink saveDrink(Drink drink);
+    Drink getDrinkById(long id);
+    List<Drink> getAllDrink();
+    Drink updateDrink(Drink drink);
+
+    void deleteDrink(long id);
+}
